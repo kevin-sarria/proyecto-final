24c979f94d5e9d6b1eefd3a7462b8dfa5a335452
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2021-10-28 11:57:16 -0500

    Add files via upload

--- instalador/v-instalador.php
+++ instalador/v-instalador.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" type = "text/css" href="../css/instalador.css">
-    <title>Instaldor</title>
+    <title>Instalador</title>
 </head>
 <body>
 <div class="con_ins">
@@ -14,7 +14,7 @@
 
             <img src="../img/logo.png" class="img_logo"><br>
 
-            <h1>Instaldor</h1>
+            <h1>Instalador</h1>
             Nombre servidor <br>
             <input type="text" name="servidor" >
             <div>Nombre usuario</div>
